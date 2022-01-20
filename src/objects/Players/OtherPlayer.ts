1128d219f01b06755a55cc64b1ddc2a7156f0364
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-01-20 15:24:25 -0800

    refactored other player movement

--- src/objects/Players/OtherPlayer.ts
+++ src/objects/Players/OtherPlayer.ts
@@ -1,13 +1,10 @@
 import { ComponentsScene } from '../../abstractions/CustomScene';
 import { Entity as Entity } from '../../abstractions/Entity';
 import { MainSceneData } from '../../constants/scenes/MainSceneData';
-import { GRID_HEIGHT, GRID_WIDTH } from '../../constants/worldConstants';
-import { geckosClientHelper } from '../../game';
 import MainScene from '../../scenes/mainScene';
 import { AnimationDirection } from '../../typings/AnimationTypes';
-import { ICameraCoordinates, IConnectedPlayer, PlayerGeckosEvents } from '../../typings/PlayerTypes';
+import { ICameraCoordinates } from '../../typings/PlayerTypes';
 import { OtherPlayerMovement } from './components/OtherPlayer/OtherPlayerMovement';
-import { PlayerCamera } from './components/Player/PlayerCamera';
 
 export class OtherPlayer extends Entity {
   private direction: AnimationDirection = 'down';
@@ -32,66 +29,15 @@ export class OtherPlayer extends Entity {
     this.id = id;
     this.name = name;
 
-    scene.components.addComponent(this, new OtherPlayerMovement());
+    scene.components.addComponent(this, new OtherPlayerMovement(this.direction));
 
     console.log(`💡 Other player(${this.name}) id ${this.id} has been created and added to position: ${x}, ${y}`);
-
-    this.handleSocketEvents();
-
-    this.scene.events.addListener('update', this.onUpdate, this); // this should be called inside the scene
-  }
-
-  public onUpdate() {
-    if (MainScene.grid.hasCharacter(this.id)) {
-      this.animations();
-
-      // delete itself if too far, to preserve memory
-      const isOnView = PlayerCamera.worldViewWithOffset.contains(this.x, this.y);
-
-      if (!isOnView) {
-        this.destroy();
-        console.log(`destroying other player out of view: ${this.id}`);
-      }
-    }
   }
 
   destroy(fromScene?: boolean): void {
     MainScene.grid.removeCharacter(this.id);
     MainScene.otherPlayers = MainScene.otherPlayers.filter((p) => p.id !== this.id);
 
-    this.scene.events.removeListener('update', this.onUpdate, this); // remove this listener, otherwise the game will crash once the object is destroyed and it tries to animate it
     super.destroy(fromScene);
-  }
-
-  public handleSocketEvents() {
-    geckosClientHelper.channel.on(PlayerGeckosEvents.PlayerPositionUpdate, (d) => {
-      const data = d as IConnectedPlayer;
-
-      if (data.id === this.id) {
-        console.log(`received position update for other player ${data.id}`);
-        console.log('pos', data);
-
-        this.x = data.x;
-        this.y = data.y;
-
-        // this.cameraCoordinates = data.cameraCoordinates; //! REACTIVATE on debug
-
-        MainScene.grid.setPosition(this.id, {
-          x: Math.round(data.x / GRID_WIDTH),
-          y: Math.round(data.y / GRID_HEIGHT),
-        });
-
-        this.direction = data.direction as AnimationDirection;
-
-        if (data.isMoving) {
-          MainScene.grid.move(this.id, data.direction);
-        }
-      }
-    });
-  }
-
-  public animations() {
-    // this avoids crashes if instance is destroyed
-    this.playAnimations(this.direction, MainScene.grid.isMoving(this.id));
   }
 }
--- src/objects/Players/components/OtherPlayer/OtherPlayerMovement.ts
+++ src/objects/Players/components/OtherPlayer/OtherPlayerMovement.ts
@@ -1,13 +1,20 @@
 import { IComponent } from '../../../../abstractions/ComponentService';
 import { GRID_HEIGHT, GRID_WIDTH } from '../../../../constants/worldConstants';
+import { geckosClientHelper } from '../../../../game';
 import MainScene from '../../../../scenes/mainScene';
 import { AnimationDirection } from '../../../../typings/AnimationTypes';
+import { IConnectedPlayer, PlayerGeckosEvents } from '../../../../typings/PlayerTypes';
 import { OtherPlayer } from '../../OtherPlayer';
+import { PlayerCamera } from '../Player/PlayerCamera';
 
 export class OtherPlayerMovement implements IComponent {
   private gameObject: OtherPlayer;
   private direction: AnimationDirection = 'down';
   private speed: number = 2;
+
+  constructor(direction: AnimationDirection) {
+    this.direction = direction;
+  }
 
   public init(targetObject: OtherPlayer) {
     this.gameObject = targetObject;
@@ -25,7 +32,48 @@ export class OtherPlayerMovement implements IComponent {
     });
   }
 
-  public start() {}
+  public start() {
+    this.onSocketReceivePositionUpdate();
+  }
 
-  public update() {}
+  public update() {
+    if (MainScene.grid.hasCharacter(this.gameObject.id) && this.gameObject.active) {
+      this.gameObject.playAnimations(this.direction, MainScene.grid.isMoving(this.gameObject.id));
+
+      // delete itself if too far, to preserve memory
+      const isOnView = PlayerCamera.worldViewWithOffset.contains(this.gameObject.x, this.gameObject.y);
+
+      if (!isOnView) {
+        console.log(`destroying other player out of view: ${this.gameObject.id}`);
+        this.gameObject.destroy();
+      }
+    }
+  }
+
+  public onSocketReceivePositionUpdate() {
+    geckosClientHelper.channel.on(PlayerGeckosEvents.PlayerPositionUpdate, (d) => {
+      const data = d as IConnectedPlayer;
+
+      if (data.id === this.gameObject.id) {
+        console.log(`received position update for other player ${data.id}`);
+        console.log('pos', data);
+
+        this.gameObject.x = data.x;
+        this.gameObject.y = data.y;
+
+        // this.cameraCoordinates = data.cameraCoordinates; //! REACTIVATE on debug
+
+        MainScene.grid.setPosition(this.gameObject.id, {
+          x: Math.round(data.x / GRID_WIDTH),
+          y: Math.round(data.y / GRID_HEIGHT),
+        });
+
+        this.direction = data.direction as AnimationDirection;
+
+        if (data.isMoving) {
+          MainScene.grid.move(this.gameObject.id, data.direction);
+        }
+      }
+    });
+  }
 }
