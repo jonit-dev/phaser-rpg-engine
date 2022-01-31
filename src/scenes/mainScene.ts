17d806ce90946511627fa52dbe774d43741feda9
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-01-30 18:10:22 -0800

    animated tiles plugin

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -1,3 +1,4 @@
+import AnimatedTiles from 'phaser-animated-tiles-phaser3.5/dist/AnimatedTiles.min.js';
 import { ComponentsScene } from '../abstractions/ComponentsScene';
 import { PLAYER_START_POS_X, PLAYER_START_POS_Y } from '../constants/playerConstants';
 import { MainSceneData } from '../constants/scenes/MainSceneData';
@@ -20,8 +21,15 @@ export default class MainScene extends ComponentsScene {
     super({ key: MainSceneData.key });
   }
 
+  preload() {
+    this.load.scenePlugin('animatedTiles', AnimatedTiles, 'animatedTiles', 'animatedTiles');
+  }
+
   create() {
     this.map = new MapLoader(this, 'default_map'); // should come before player rendering, to avoid overlap
+    //@ts-ignore
+    this.sys.animatedTiles.init(this.map.tilemap);
+
     this.physics.world.setBounds(0, 0, WORLD_WIDTH, WORLD_HEIGHT); //map size limits
     new GridManager(this, this.gridEngine, this.map.tilemap);
     this.player = new Player(
